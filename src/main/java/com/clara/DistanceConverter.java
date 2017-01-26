9abba08124c5d94b870b498c402a39cb29780ea7
tq1499vg <mash4peace>
2017-01-25 20:06:59 -0600

    I have fix4ed meter to feet bug.

--- src/main/java/com/clara/DistanceConverter.java
+++ src/main/java/com/clara/DistanceConverter.java
@@ -24,7 +24,7 @@ public class DistanceConverter {
         //TODO! This method doesn't work yet. Can you fix it?
         //To convert meters to feet, multiply by 3
 
-        return 0;
+        return  3* meters;
     }
 
     // I know doubles would be more real world but we are keeping it simple
